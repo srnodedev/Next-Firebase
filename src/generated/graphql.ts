f6626b4f711f117e9a12af226edd3490164069c9
srnodedev <117995564+srnodedev@users.noreply.github.com>
2022-11-22 13:12:38 -0300

    Update graphql.ts

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 import * as Urql from 'urql'
 export type Maybe<T> = T | null
 export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K]
+  [K in keyof T]: T[K] 
 }
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
   { [SubKey in K]?: Maybe<T[SubKey]> }
