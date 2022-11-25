bb2e2b61d5d0512be8df188249883c9c128e1bae
srnodedev <117995564+srnodedev@users.noreply.github.com>
2022-11-25 13:59:18 -0300

    Update [city_id].tsx

--- pages/city/[city_id].tsx
+++ pages/city/[city_id].tsx
@@ -1,6 +1,6 @@
 import { gql } from 'graphql-request'
 import type { GetStaticPaths, GetStaticProps } from 'next'
-import { NextSeo } from 'next-seo'
+import { NextSeo } from 'next-seo' 
 import type { CityDetailProps } from '../../src/components/city-detail/city-detail'
 import CityDetail from '../../src/components/city-detail/city-detail'
 import { hasuraServerRequest } from '../../src/utils/hasura/hasura-server-request'
