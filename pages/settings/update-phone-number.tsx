09f2e14c9a957c00d832ef248c2afe3d858272df
srnodedev <117995564+srnodedev@users.noreply.github.com>
2022-12-01 07:21:27 -0500

    Update update-phone-number.tsx

--- pages/settings/update-phone-number.tsx
+++ pages/settings/update-phone-number.tsx
@@ -1,7 +1,7 @@
 import { NextSeo } from 'next-seo'
 
 import SettingsUpdatePhoneNumber from '../../src/components/settings-update-phone-number/settings-update-phone-number'
-
+ 
 type SettingsUpdatePhoneNumberPageProps = unknown
 
 function SettingsUpdatePhoneNumberPage(_: SettingsUpdatePhoneNumberPageProps) {
